87fadccd58706c98da2c9353af5780c5b92ef84a
YANGGMM <18916107305@163.com>
2023-05-25 16:34:22 +0800

    fix bvt test

--- test/distributed/cases/transaction/transaction1.sql
+++ test/distributed/cases/transaction/transaction1.sql
@@ -40,3 +40,17 @@ select * from atomic_table_10;
 select * from atomic_table_10;
 commit;
 select * from atomic_table_10;
+
+drop table if exists atomic_table_11;
+create table atomic_table_11(c1 int,c2 varchar(25));
+insert into atomic_table_11 values (3,"a"),(4,"b"),(5,"c");
+begin;
+drop table atomic_table_11;
+-- @bvt:issue#9429
+-- @session:id=2&user=sys:dump&password=111
+insert into atomic_table_11 values (6,"a");
+select * from atomic_table_11;
+-- @session
+-- @bvt:issue
+commit;
+select * from atomic_table_11;
