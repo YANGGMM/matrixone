4a4f3f186329398de5f33de28225ad4f3d893b93
YANGGMM <18916107305@163.com>
2024-09-10 11:13:05 +0800

    fix type cast for bigint unsigned

--- pkg/sql/plan/function/type_check.go
+++ pkg/sql/plan/function/type_check.go
@@ -592,7 +592,7 @@ func initFixed1() {
 		{types.T_uint64, types.T_int8, types.T_uint64, types.T_uint64},
 		{types.T_uint64, types.T_int16, types.T_uint64, types.T_uint64},
 		{types.T_uint64, types.T_int32, types.T_uint64, types.T_uint64},
-		{types.T_uint64, types.T_int64, types.T_int64, types.T_int64},
+		{types.T_uint64, types.T_int64, types.T_uint64, types.T_uint64},
 		{types.T_uint64, types.T_uint8, types.T_uint64, types.T_uint64},
 		{types.T_uint64, types.T_uint16, types.T_uint64, types.T_uint64},
 		{types.T_uint64, types.T_uint32, types.T_uint64, types.T_uint64},
